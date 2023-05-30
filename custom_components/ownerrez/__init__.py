f958bb439d9bf45b76b6236a8365b9b146799249
blueflame66 <glenn.moore@gmail.com>
2023-05-29 20:58:07 -0400

    Objects

--- custom_components/ownerrez/__init__.py
+++ custom_components/ownerrez/__init__.py
@@ -1,11 +1,11 @@
-"""The OwnerRez Integration integration."""
+"""The OwnerRez Integration integration2."""
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.const import CONF_API_TOKEN, CONF_USERNAME, CONF_DELAY_TIME
-from .const import CONF_DAYS, CONF_MAX_EVENTS, DEFAULT_URL
+from .const import CONF_DAYS, CONF_MAX_EVENTS, DEFAULT_URL, DEFAULT_URL_V2
 from homeassistant.components.calendar import CalendarEvent
 from homeassistant.util import Throttle, dt
 from datetime import datetime, timedelta, date
@@ -31,7 +31,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     auth = aiohttp.BasicAuth(config.get(CONF_USERNAME), config.get(CONF_API_TOKEN))
     domain_entry = {
         "auth": auth,
-        # CONF_DELAY_TIME: config.get(CONF_DELAY_TIME),
         CONF_DAYS: config.get(CONF_DAYS),
         CONF_MAX_EVENTS: config.get(CONF_MAX_EVENTS),
     }
@@ -56,14 +55,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             jre2.append(paul)
 
             hass.data[DOMAIN]["Calendars"] = []
+            hass.data[DOMAIN]["Properties"] = []
             CalPointer = hass.data[DOMAIN]["Calendars"]
+            CalPointer2 = hass.data[DOMAIN]["Properties"]
+            CalPointer3 = hass.data[DOMAIN]
 
     for orproperty in jre2:
         CalPointer.append(CalEvents(config, orproperty))
-        # hass.data[DOMAIN][orproperty["Key"]] = CalEvents(config, orproperty)
-        # glenn7 = hass.data[DOMAIN][orproperty["Key"]]
-        # await glenn7.update()
-        # glenn8 = glenn7
+        CalPointer2.append(OR_Property(orproperty, config, auth))
+
+    for orprop in hass.data[DOMAIN]["Properties"]:
+        await orprop.retrieve_bookings()
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
@@ -76,6 +78,129 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+class OR_Property:
+    """Property Object from OR"""
+
+    def __init__(self, prop, config, auth) -> None:
+        self._key = prop["Key"]
+        self._id = prop["Id"]
+        self._name = prop["Name"]
+        self._tzid = prop["TimeZoneId"]
+        self._auth = auth
+        self.days = int(config.get(CONF_DAYS))
+        self.bookings = []
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    async def retrieve_bookings(self) -> None:
+        """Get the Bookings for the Property"""
+        self.bookings = []
+
+        start_of_events = dt.start_of_local_day() - timedelta(days=60)
+        end_of_events = dt.start_of_local_day() + timedelta(days=self.days)
+        current_date = dt.now()
+        events_start = start_of_events.strftime("%Y-%m-%d")
+        events_end = end_of_events.strftime("%Y-%m-%d")
+        date_format = "%Y-%m-%dT%H:%M:%S"
+        tz = pytz.timezone(self._tzid)
+
+        api_url = (
+            DEFAULT_URL_V2
+            + "/bookings?property_ids="
+            + str(self.id)
+            + "&from="
+            + events_start
+            + "&to="
+            + events_end
+        )
+
+        async with aiohttp.ClientSession(auth=self._auth) as session:
+            async with session.get(api_url) as resp:
+                if resp.status == 200:
+                    jresult = await resp.json()
+
+                    for bookingrecord in jresult["items"]:
+                        if bookingrecord["is_block"] == False:
+                            api_url = (
+                                DEFAULT_URL_V2 + "/bookings/" + str(bookingrecord["id"])
+                            )
+
+                            async with session.get(api_url) as resp:
+                                bresult = await resp.json()
+
+                            nr_datestr = (
+                                bookingrecord["arrival"]
+                                + "T"
+                                + bresult["check_in"]
+                                + ":00"
+                            )
+
+                            nr_start_dt = datetime.strptime(nr_datestr, date_format)
+                            nr_start = tz.localize(nr_start_dt)
+
+                            nr_datestr = (
+                                bookingrecord["departure"]
+                                + "T"
+                                + bresult["check_out"]
+                                + ":00"
+                            )
+
+                            nr_end_dt = datetime.strptime(nr_datestr, date_format)
+                            nr_end = tz.localize(nr_end_dt)
+
+                            api_url = (
+                                DEFAULT_URL_V2
+                                + "/guests/"
+                                + str(bookingrecord["guest_id"])
+                            )
+
+                            async with session.get(api_url) as resp:
+                                guest_name = await resp.json()
+
+                                booking_record = {
+                                    "start": nr_start,
+                                    "end": nr_end,
+                                    "summary": guest_name["first_name"]
+                                    + " "
+                                    + guest_name["last_name"]
+                                    + " TZ:"
+                                    + tz.zone,
+                                    "guestname": guest_name["first_name"]
+                                    + " "
+                                    + guest_name["last_name"],
+                                    "orid": self.id,
+                                }
+
+                                self.bookings.append(Booking(booking_record))
+
+        await session.close()
+
+    @property
+    def key(self):
+        """Return the next upcoming event."""
+        return self._key
+
+    @property
+    def id(self):
+        """Return the next upcoming event."""
+        return self._id
+
+    @property
+    def name(self):
+        """Return the next upcoming event."""
+        return self._name
+
+
+class Booking:
+    """Object to Represent a booking"""
+
+    def __init__(self, booking_id) -> None:
+        self._booking_id = booking_id
+        self._guest_name = "Test"
+        self._arrival = None
+        self._departure = None
+        self._cleaning = None
 
 
 class CalEvents:
@@ -215,3 +340,52 @@ class CalEvents:
                             }
                             self.calendar.append(new_record)
         await session.close()
+
+class OR_UpdateCoordinator(DataUpdateCoordinator):
+    """Class to manage fetching update data from single endpoint."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        api: OmniLogic,
+        name: str,
+        config_entry: ConfigEntry,
+        polling_interval: int,
+    ) -> None:
+        """Initialize the global Omnilogic data updater."""
+        self.api = api
+        self.config_entry = config_entry
+        self._last_data = None
+        self._timeout_count = 0
+
+        super().__init__(
+            hass=hass,
+            logger=_LOGGER,
+            name=name,
+            update_interval=timedelta(seconds=polling_interval),
+        )
+
+    async def _async_update_data(self):
+    """Fetch data from Owner Res."""
+    try:
+        async with async_timeout.timeout(30):
+            data = await self.api.get_telemetry_data()
+
+        self._timeout_count = 0
+
+    except OmniLogicException as error:
+        raise UpdateFailed(f"Error updating from OmniLogic: {error}") from error
+
+    except LoginException as error:
+        raise UpdateFailed(f"Login failed for Omnilogic: {error}") from error
+
+    except TimeoutError as error:
+        self._timeout_count += 1
+
+        if self._timeout_count > 10 or not self._last_data:
+            raise UpdateFailed(f"Timeout updating OmniLogic from cloud: {error}") from error
+        else:
+            data = self._last_data
+
+    parsed_data = {}
+    self._last_data = data
--- custom_components/ownerrez/const.py
+++ custom_components/ownerrez/const.py
@@ -6,5 +6,7 @@ CONF_MAX_EVENTS = "max_events"
 CONF_DAYS = "days"
 
 DEFAULT_URL = "https://secure.ownerreservations.com/api"
+DEFAULT_URL_V2 = "https://api.ownerreservations.com/v2"
+
 DEFAULT_PT = "pt_xxx"
 DEFAULT_UN = "emailaddress"
